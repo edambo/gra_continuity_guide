2133cddec176a5ed0fdbdbfe62e5fa0d1f6a2ab4
Ebie Dambo <ebiekiemiete@gmail.com>
2024-08-26 10:32:47 -0500

    Create book

---
+++ R/highlight.R
@@ -0,0 +1,10 @@
+# Highlight text using grey background
+
+highlight <- function(x, color = "#ebebeb") {
+  if (knitr::is_latex_output()) {
+    sprintf("\\textcolor{%s}{%s}", color, x)
+  } else if (knitr::is_html_output()) {
+    sprintf("<span style='color: %s;'>%s</span>", color,
+            x)
+  } else x
+}
